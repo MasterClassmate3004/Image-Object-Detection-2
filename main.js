3bf3eff61f821cc5ae3a2d6f8f6532f298938d35
MasterClassmate3004 <77794181+MasterClassmate3004@users.noreply.github.com>
2021-08-31 16:46:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+status = "";
+
+preload()
+{
+    Fan = loadImage(fan.jpg);
+}
+
+function setup()
+{
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    object.detect(video, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+}
